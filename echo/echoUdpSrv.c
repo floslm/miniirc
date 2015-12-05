62fc2c3b22c16e63d6110b92dab1f665624bcd5e
Pascal Emile Fares <pfares@cofares.net>
2015-12-05 11:46:29 +0200

    Debugginfg et refactoring

--- echo/echoUdpSrv.c
+++ echo/echoUdpSrv.c
@@ -1,102 +1,63 @@
 #include<stdio.h> 
-
 #include<sys/types.h> 
-
 #include<netinet/in.h>
-
 #include<sys/socket.h> 
-
 #include<string.h> 
-
 #include<unistd.h> 
-
 #include<stdlib.h>
 
 #define PORT 7844
 
-int main(int argc,char *argv[]) {
+int main(int argc, char *argv[]) {
+    char buf[2000];
+    int sockfd, len, a;
+    struct sockaddr_in servaddr, cliaddr;
 
-char buf[2000];
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        printf("Erreur création socket\n");
+        exit(1);
+    }
+    printf("Socket UDP créé.\n");
 
-int sockfd,len,a;
+    bzero(&servaddr, sizeof (servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-struct sockaddr_in servaddr,cliaddr;
-
-if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0)
-
-{
-
-    printf("Error creating socket\n"); 
-
-    exit(0);
-
-}
-printf("UDP Server Socket Created Successfully.\n"); 
-
-bzero(&servaddr,sizeof(servaddr));
-
-servaddr.sin_family=AF_INET;
-
-servaddr.sin_port=htons(PORT); 
-
-servaddr.sin_addr.s_addr=htonl(INADDR_ANY); 
-
-if(bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr))<0)
-
-{
-
-    perror("Error binding socket."); 
-
-    exit(0);
-
-}
-
-printf("UDP Server Socket Binded.\n"); 
-
-len=sizeof(cliaddr);
-
-do {
-
-    strcpy(buf," "); 
-
-    if((a=recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr *)&cliaddr,&len))<0){
-
-        perror("ERROR"); 
-        exit(0);
-
+    if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr)) < 0) {
+        perror("Erreur bind socket.");
+        exit(2);
     }
 
-    printf("From Client : %s",buf); 
+    printf("UDP Serveur associé à Socket.\n");
+
+    len = sizeof (cliaddr);
+
+    do {
+
+        bzero(buf, 2000);
+
+        if ((a = recvfrom(sockfd, buf, sizeof (buf), 0, (struct sockaddr *) &cliaddr, &len)) < 0) {
+
+            perror("ERREUR recvfrom");
+            exit(3);
+
+        }
+
+        printf("reću du Client : %s", buf);
 
 
-    if((sendto(sockfd,buf,a,0,(struct sockaddr *)&cliaddr,len))<0)
+        if ((sendto(sockfd, buf, a, 0, (struct sockaddr *) &cliaddr, len)) < 0) {
+            perror("Rien envoyé");
 
-    {
+            exit(4);
 
-        perror("NOTHING SENT"); 
+        }
 
-        exit(0);
+    } while (buf[0] != '.');
 
-    }
-    printf("Server : ");
-    fgets(buf,sizeof(buf),stdin);
+    close(sockfd);
 
-    if((sendto(sockfd,buf,a,0,(struct sockaddr *)&cliaddr,len))<0)
-
-    {
-
-        perror("NOTHING SENT"); 
-
-        exit(0);
-
-    }
-
-
-} 
-while(strcmp(buf," ")!=0); 
-
-close(sockfd); 
-
-return 0;
+    return 0;
 
 }
